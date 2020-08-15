ee6cb683234906b6af203091f9bcc58a02705089
rafaasimi <rafaasimi@hotmail.com>
2020-08-15 16:52:27 -0300

    Add dropdown-menu and menu-mobile

---
+++ js/modules/dropdown-menu.js
@@ -0,0 +1,30 @@
+import outsideClick from './outsideclick.js';
+
+export default function initDropdownMenu() {
+
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]')
+
+    dropdownMenus.forEach((menu) => {
+
+        // Adicionar + de um evento
+        // Array ou adicionando 2 addEventListener
+        // Click = Demora 300ms para ser executado no Mobile
+        // TouchStart = Inicia na hora
+        ['touchstart', 'click'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick);
+        })
+
+    })
+
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add('active')
+        outsideClick(this, ['touchstart', 'click'], () => {
+            this.classList.remove('active')
+
+        })
+    }
+
+
+}
+
--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tabnav.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
+import initDropdownMenu from './modules/dropdown-menu.js'
+import initMenuMobile from './modules/menu-mobile.js'
 
 
 initScrollSuave();
@@ -12,3 +14,5 @@ initAccordion();
 initTabNav();
 initModal();
 initTooltip();
+initDropdownMenu();
+initMenuMobile();
