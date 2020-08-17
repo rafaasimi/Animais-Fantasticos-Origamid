151dedbdfb0a7c5e40731c1df1a78c6cf825400c
rafaasimi <rafaasimi@hotmail.com>
2020-08-17 09:25:45 -0300

    Create new section numbers and animations.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
 import initDropdownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
+import initAnimaNumeros from './modules/anima-numeros.js'
+
 
 
 initScrollSuave();
@@ -16,3 +18,4 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
+initAnimaNumeros();
